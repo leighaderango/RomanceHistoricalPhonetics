a01944cf25c5b667fc55fdc6568cfe797274a431
Leigha <leighaderango@gmail.com>
2025-04-21 20:01:56 -0500

    read in two consonant data and added version dropdown to app

--- excel_to_tables.py
+++ excel_to_tables.py
@@ -1,15 +1,19 @@
 import pandas as pd
+import numpy as np
 
 excel_file = pd.ExcelFile('QDFH.xlsx')
 
 sheet_names = excel_file.sheet_names
 data_sheets = sheet_names[1:-4]
+selected = np.r_[0:31, 55:76]
+# 90 - end
+singles = [data_sheets[i] for i in selected]
 
 
 # single consonant treatments
 single_treatment_tables = []
 
-for sheet_name in data_sheets[0:33]: #[0:33]
+for sheet_name in singles: #[0:33]
 
         df = excel_file.parse(sheet_name) 
         print(f"Sheet: {sheet_name}")
@@ -34,6 +38,7 @@ for sheet_name in data_sheets[0:33]: #[0:33]
         
         pivot.insert(0, 'treatment', treatment)
         pivot.insert(1, 'environment', environment)
+        pivot.insert(2, 'position', 1)
 
         # clean language names and empty rows
         pivot['Languages'] = pivot['Languages'].str.replace('\n', ' ', regex = False)
@@ -44,6 +49,67 @@ for sheet_name in data_sheets[0:33]: #[0:33]
 
 all_df = pd.concat(single_treatment_tables, axis = 0)
 
-all_df.to_csv('single_consonants_data.csv')
+#all_df.to_csv('single_consonants_data.csv')
 
+
+### two positions
+
+doubles = np.r_[31:47, 77:93]
+doubles = [data_sheets[i] for i in doubles]
+
+double_treatment_tables = []
+n_consonants = 2
+
+for sheet_name in doubles: 
+
+        df = excel_file.parse(sheet_name) 
+        print(f"Sheet: {sheet_name}")
+
+
+        # get basic info
+        number = df.iloc[1,1]
+        treatment = df.iloc[2,1]
+        environment = df.iloc[3, 1]
+
+        treatment_tables = []
+        
+        for i in range(0, n_consonants): 
+                if i == 0:
+                        pos_table = df.iloc[5:17, :]
+                elif i == 1:
+                        pos_table = df.iloc[np.r_[5, 18:29], :]
+                
+                # pivot table so rows = cols
+                melted =  pos_table.melt(id_vars = 'A - Consonantism', var_name = 'Variable', value_name = 'Value')
+                pivot = melted.pivot(index = 'Variable', columns = 'A - Consonantism', values = 'Value')
+                pivot.set_index('Languages', inplace = True)
+                pivot.reset_index(inplace = True)
+
+                
+                # deal with a/b/c versions
+                pivot['version'] = pivot.groupby('Languages').cumcount().add(1)
+
+                
+                pivot.insert(0, 'treatment', treatment)
+                pivot.insert(1, 'environment', environment)
+                pivot.insert(2, 'position', i+1)
+
+                # clean language names and empty rows
+                pivot['Languages'] = pivot['Languages'].str.replace('\n', ' ', regex = False)
+                pivot = pivot.dropna(subset = ['Languages'])
+
+                treatment_tables.append(pivot)
+        
+        double_treatment_table = pd.concat(treatment_tables, axis = 0)
+        double_treatment_table.sort_values(['Languages'], inplace =True)
+        double_treatment_tables.append(double_treatment_table)
+
+
+double_treatment = pd.concat(double_treatment_tables, axis = 0)
+
+ 
+# put all_df with double_treatments 
+all_consonants_data = pd.concat([all_df, double_treatment], axis = 0)
+
+all_consonants_data.to_csv('all_consonants_data.csv')
 
--- map_trial.py
+++ map_trial.py
@@ -7,7 +7,8 @@ cities = pd.read_csv('languageCities.csv', encoding = 'utf-8')
 
 cities_geo = gpd.GeoDataFrame(cities, geometry = gpd.points_from_xy(cities['latitude'], cities['longitude']))
 
-latin_to_rom = pd.read_csv('single_consonants_data.csv')
+latin_to_rom = pd.read_csv('all_consonants_data.csv')
+
 
 app = Dash()
 
@@ -20,13 +21,22 @@ app.layout = html.Div(children=[
 
     html.Div(children = [
         html.Label('Treatment'),
-        dcc.Dropdown(latin_to_rom['treatment'].unique().tolist(), id = 'select_treatment')
+        dcc.Dropdown(latin_to_rom['treatment'].unique().tolist(),
+                     value = '-R-',
+                     id = 'select_treatment')
         ]),
 
     html.Div(children = [
         html.Label('Environment'),
-        dcc.Dropdown(id = 'select_environment')
+        dcc.Dropdown(value = 'V_V', 
+                     id = 'select_environment')
     ]),
+
+    html.Div(children = [
+             html.Label('Version'),
+             dcc.Dropdown(value = 1,
+                          id = 'select_version')
+    ]),    
 
     dcc.Graph(
         id='map'
@@ -41,31 +51,59 @@ def update_environments(treatment):
     environ_list = latin_to_rom[latin_to_rom['treatment'] == treatment]['environment'].unique().tolist()
     return environ_list
 
+@callback(
+    Output('select_version', 'options'),
+    Input('select_treatment', 'value'),
+    Input('select_environment', 'value')
+)
+def update_versions(treatment, environment):
+    versions_list = latin_to_rom[(latin_to_rom['treatment'] == treatment) & (latin_to_rom['environment'] == environment)]['version'].unique().tolist()
+    return versions_list
+
 
 @callback(
     Output('map', 'figure'),
     Input('select_treatment', 'value'),
-    Input('select_environment', 'value'))
-def update_map(selected_treatment, selected_environment):
-    latin_to_rom_sub = latin_to_rom[(latin_to_rom['treatment'] == selected_treatment) & (latin_to_rom['environment'] == selected_environment)]
-
-    city_context = pd.merge(cities_geo, latin_to_rom_sub, left_on = 'Language', right_on = 'Languages').dropna(subset = 'IPA')
+    Input('select_environment', 'value'),
+    Input('select_version', 'value'))
+def update_map(selected_treatment, selected_environment, selected_version):
+    latin_to_rom_sub = latin_to_rom[(latin_to_rom['treatment'] == selected_treatment) &
+                                     (latin_to_rom['environment'] == selected_environment)]
     
-    city_context = city_context[city_context['version'] == 1]
+    grouped = latin_to_rom_sub.groupby('Languages')
+
+    version_tables = []
+    for language, df in grouped:
+        if selected_version in df['version'].unique().tolist():
+            select_version = selected_version
+        else:
+            select_version = 1
+        
+        lang_table = df[df['version'] == select_version]
+        version_tables.append(lang_table)
+
+    if version_tables:
+        version_sub = pd.concat(version_tables, axis=0)
+    else:
+        version_sub = pd.DataFrame(columns=latin_to_rom.columns)
+
+    city_context = pd.merge(cities_geo, version_sub, left_on = 'Language', right_on = 'Languages').dropna(subset = 'IPA')
+    
 
     fig = px.scatter_geo(city_context,
                     lat=city_context.geometry.x,
                     lon=city_context.geometry.y, 
                     hover_name = 'Language Variety', 
-                    center = dict(lat = 45.76, lon = 4.84), 
                     text = 'IPA')
 
-    fig.update_layout(geo = dict(projection_scale = 8))
+    fig.update_layout(geo = dict(projection_scale = 8,
+                                 center = dict(lat = 45.76, lon = 4.84)))
     fig.update_traces(textfont = dict(family = 'Arial',
                                     size = 16,
                                     color = 'black'),
                         marker = dict(size = 30,
                                       opacity = 0.5))
+    fig.update_geos(showcountries = True)
     return fig
 
 
